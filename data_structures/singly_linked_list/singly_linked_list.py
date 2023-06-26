8237e0424c8b5661f76e03c09004b524b87232bb
jdboachie <judeboachiekwame@gmail.com>
2023-06-26 13:17:23 +0000

    chore: Update structure

--- data_structures/singly_linked_list/singly_linked_list.py
+++ data_structures/singly_linked_list/singly_linked_list.py
@@ -118,6 +118,29 @@ class LinkedList:
 
         self.head = _reverse_recursive(cur=self.head, prev=None)
 
+    def reverse_iterative(self, ):
+        prev = None
+        current = self.head
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+        self.head = prev
+
+    def reverse_recursive(self, ):
+
+        def _reverse_recursive(cur, prev):
+            if not cur:
+                return prev
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+            return _reverse_recursive(cur, prev)
+
+        self.head = _reverse_recursive(cur=self.head, prev=None)
+
     def swap_nodes(self, key_1, key_2):
 
         if key_1 == key_2:
@@ -157,21 +180,6 @@ llist.append("B")
 llist.append("C")
 llist.append("D")
 
-print("Original List")
-llist.print_list()
+llist.reverse_iterative()
 
-llist.swap_nodes("B", "C")
-print("Swapping nodes B and C that are not head nodes")
-llist.print_list()
-
-llist.swap_nodes("A", "B")
-print("Swapping nodes A and B where key_1 is head node")
-llist.print_list()
-
-llist.swap_nodes("D", "B")
-print("Swapping nodes D and B where key_2 is head node")
-llist.print_list()
-
-llist.swap_nodes("C", "C")
-print("Swapping nodes C and C where both keys are same")
 llist.print_list()
